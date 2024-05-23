8e805f49300885f473ca3a2580a74ba8d66d2348
someSkunkFunk <dersfocused@gmail.com>
2024-05-23 16:33:43 -0400

    simulated for multiple k values

--- wc_sim.py
+++ wc_sim.py
@@ -1,5 +1,7 @@
 #%%
 # INIT
+# see Lotka-volterra example from 
+# https://ulissigroup.cheme.cmu.edu/math-methods-chemical-engineering/notes/ordinary_differential_equations/18-nonlinear-coupled-ODEs.html
 from scipy.integrate import solve_ivp
 # ivp := "initial value problem"
 import numpy as np
@@ -11,27 +13,6 @@ with open(env_loc,"rb") as file:
   envs=pickle.load(file)
 
 #%%
-# Lotka-volterra example from 
-# https://ulissigroup.cheme.cmu.edu/math-methods-chemical-engineering/notes/ordinary_differential_equations/18-nonlinear-coupled-ODEs.html
-alpha = 1 # 1/day
-beta = 0.2 # 1/wolves/day
-delta = 0.5 # 1/rabbits/day
-gamma = 0.2 # 1/day
-
-def diffeq(t,pop):
-  x,y = pop
-  return [alpha*x-beta*x*y,
-          delta*x*y-gamma*y]
-
-sol = solve_ivp(diffeq, 
-                t_span = [0,40],
-                y0=[1,5],
-                t_eval=np.linspace(0,40,100))
-
-plt.plot(sol.t, sol.y.T,'-')
-plt.legend(['Rabbits','Wolves'])
-plt.xlabel('Time [days]')
-plt.ylabel('Population [#]')
 #%%
 # Wilson Cowan "classic"
 # goal: recreate  Fig S2 in Doelling & Assaneo 2023
@@ -45,11 +26,13 @@ d=-2 # I think this is the same as g...? typo? or I'm stupid?
 rho_E=2.3
 rho_I=-3.2
 tau=66e-3 # in seconds (66 ms)
-k=1.5
-
+ks=[1.5, 100, 0.03, 7]
 #
 fs=100 #hz
+# first sentence as test (already downsampled to 100 Hz)
 test_env=envs['b01s01p01']
+t_end=len(test_env)/fs # end time
+t_input=np.arange(len(test_env))/fs # input time vector for plotting
 def sigmoid(z):
   return 1/(1+np.exp(-z))
 def A(t):
@@ -71,28 +54,36 @@ def wc_deq(t,neural_activity):
 
 #%%
 # EVAL
-t_end=len(test_env)/fs
-sol = solve_ivp(wc_deq, 
-                t_span = [0,t_end],
-                y0=[0.25,0.75],
-                t_eval=np.linspace(0,t_end,1000))
 
+#set manually by plotting them and seeing which would give enough space to actually see the lines
 
-#%%
-# plot output
-E_sol=sol.y[0,:]
-I_sol=sol.y[1,:]
-out_sol=(E_sol-I_sol).T
-t_input=np.arange(len(test_env))/fs
-plt.plot(t_input,test_env)
-plt.plot(sol.t, out_sol) 
-plt.legend(['Speech Envelope','Wilson Cowan Output'])
-plt.xlabel('Time (s)')  
-plt.title('Wilson Cowan Simulation')
-# plt.ylabel('Neural Activity')
-#%%
-# plot solution
-plt.plot(sol.t, sol.y.T,'-') 
-plt.legend(['Excitatory', 'Inhibitory'])
-plt.xlabel('Time (s)')
-plt.ylabel('Neural Activity')
+ylims=[[(-0.75,1),(0,1.3)],[(-0.75,0.75),(0,1.1)],[(-0.7,1),(0,1.1)],[(-0.8,1),(0,1.3)]]
+for i_k, k in enumerate(ks):
+  print(f'solving for k={k}...')
+  sol=solve_ivp(wc_deq, 
+                  t_span = [0,t_end],
+                  y0=[0.25,0.75],
+                  t_eval=np.linspace(0,t_end,1000))
+
+  E_sol=sol.y[0,:]
+  I_sol=sol.y[1,:]
+  out_sol=(E_sol-I_sol).T
+
+  # plot output
+  fig, axs=plt.subplots(2,1,sharex=True,figsize=(16,12))
+  
+  axs[0].plot(t_input,test_env)
+  axs[0].plot(sol.t, out_sol) 
+  axs[0].legend(['Speech Envelope','Wilson Cowan Output (E-I)'])
+  # axs[0].set_xlabel('Time (s)') 
+  axs[0].set_title(f'Wilson Cowan Simulation k={k}')
+  axs[0].set_ylim(ylims[i_k][0])
+  # plt.ylabel('Neural Activity')
+
+  # plot solution
+  axs[1].plot(sol.t, sol.y.T,'-') 
+  axs[1].legend(['Excitatory', 'Inhibitory'])
+  axs[1].set_xlabel('Time (s)')
+  axs[1].set_ylabel('Neural Activity (au)')
+  axs[1].set_title(f'Wilson Cowan simulated response, k={k}')
+  axs[1].set_ylim(ylims[i_k][1])
